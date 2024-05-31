b4ed0aaeb6ca9e65a7fd625016baf4f8e066ab33
MusingaBrian <musingabrians@gmail.com>
2024-05-31 19:26:03 +0300

    Add the modal class

--- routes/web.php
+++ routes/web.php
@@ -1,55 +1,23 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
 Route::get('jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '900,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '500,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '350,000',
-            ],
-        ],
+        'jobs' => Job::all(),
 
     ]);
 })->name('jobs');
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '900,000',
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '500,000',
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '350,000',
-        ],
-    ];
 
-    $job = Arr::first($jobs, fn ($job) => $job['id'] == $id);
-
+    $job = Job::find($id);
+    // dd($job);
     return view('job', ['job' => $job]);
 })->name('job');
 
