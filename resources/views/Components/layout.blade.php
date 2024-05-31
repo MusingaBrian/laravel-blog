decce3f586b85e565c2f4f293587c22cf4b95996
MusingaBrian <musingabrians@gmail.com>
2024-05-31 04:09:04 +0300

    Add active state to the navigation links

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +8,7 @@
     <title>App</title>
 </head>
 
-<body>
+<body class="h-full">
     <div class="min-h-full">
         <x-navbar></x-navbar>
         <x-heading>{{ $heading }}</x-heading>
